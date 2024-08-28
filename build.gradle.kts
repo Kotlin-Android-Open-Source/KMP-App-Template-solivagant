b4928e6561d0872b079f17f20ee9b84042c5a348
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2024-08-28 16:59:04 +0700

    add org.jetbrains.kotlin.plugin.compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.kotlinParcelize) apply false
+    alias(libs.plugins.kotlinCompose) apply false
 }
